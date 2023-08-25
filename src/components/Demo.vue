f6a894a64d9258c056b74bd13e9a9434fce73a28
Zhao Chen <zhao1995.chen@gmail.com>
2023-08-25 14:38:04 +0800

    feat: demo

---
+++ src/components/Demo.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { ref, version as vueVersion, reactive } from "vue";
+import { version as epVersion } from "element-plus";
+import { ElementPlus } from "@element-plus/icons-vue";
+
+type Size = "small" | "default" | "large";
+const size = ref<Size>("default");
+
+const form = reactive({
+  region: "",
+});
+
+/** The following is the setting I put in src/assets/index.scss
+@use 'sass:map';
+@use 'element-plus/theme-chalk/src/mixins/function.scss' as *;
+
+@forward 'element-plus/theme-chalk/src/common/var.scss' with (
+  $common-component-size: (
+    'large': 50px,
+    'default': 30px,
+    'small': 10px,
+  ),
+);
+*/
+</script>
+
+<template>
+  <h1>
+    <el-icon color="var(--el-color-primary)"><ElementPlus /></el-icon>
+    Element Plus {{ epVersion }} + Vue {{ vueVersion }}
+  </h1>
+  <el-divider></el-divider>
+  <p>COMPONENT OPTIONS</p>
+  <el-form label-width="80px">
+    <el-form-item label="Size" class="flex items-center">
+      <el-radio-group v-model="size">
+        <el-radio label="small">Small</el-radio>
+        <el-radio label="default">Default</el-radio>
+        <el-radio label="large">Large</el-radio>
+      </el-radio-group>
+    </el-form-item>
+  </el-form>
+  <el-divider></el-divider>
+  <p>Give size by el-form.</p>
+  <el-form :model="form" label-width="160px" :size="size">
+    <el-form-item label="Activity zone">
+      <el-select v-model="form.region" placeholder="please select your zone">
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+  </el-form>
+  <el-divider></el-divider>
+  <p>Give size by el-form-item.</p>
+  <el-form-item label="Activity zone" :size="size">
+    <el-select v-model="form.region" placeholder="please select your zone">
+      <el-option label="Zone one" value="shanghai" />
+      <el-option label="Zone two" value="beijing" />
+    </el-select>
+  </el-form-item>
+  <el-divider></el-divider>
+  <p>Give size by el-select.</p>
+  <el-select
+    v-model="form.region"
+    placeholder="please select your zone"
+    :size="size"
+  >
+    <el-option label="Zone one" value="shanghai" />
+    <el-option label="Zone two" value="beijing" />
+  </el-select>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import path from "node:path"
+import { defineConfig } from "vite"
+import Components from "unplugin-vue-components/vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      imports: ["vue", "vue-router"],
+      dirs: ["./src/components/**"],
+      dts: true
+    }),
+    Components({
+      extensions: ["vue"],
+      dirs: ["./src/components/**"],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass"
+        })
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        /**
+         * 順序會影響編譯
+         */
+        additionalData: `
+          @use "@/assets/index.scss" as *;
+          @use 'element-plus/theme-chalk/src/dark/css-vars.scss' as *;
+        `
+      }
+    }
+  }
+});
