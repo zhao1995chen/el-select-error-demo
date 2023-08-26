430133c9aa5fd2a6c55c546aa185304ebadb5cce
Zhao Chen <zhao1995.chen@gmail.com>
2023-08-27 03:41:00 +0800

    fix: after build successfully, github pages couldn't find project

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/el-select-error-demo/',
+  base: '/element-plus-error-and-solution-demo/', // 就是這個忘了改所以 trace 不到...
   plugins: [
     vue(),
     AutoImport({
